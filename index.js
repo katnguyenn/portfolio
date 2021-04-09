ef97bb6e1d7a21576e3dd150a59b70d08e814b18
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-04-09 13:19:11 -0700

    set up hamburger menu for mobile view

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 $(window).on('hashchange', function(e){
     history.replaceState ("", document.title, window.location.pathname);
 });
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
